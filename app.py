bdae8840160e69007207b2f3dfbc16e1f54ff993
Phillip Logan <philo3194@gmail.com>
2023-02-14 18:39:28 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+  return { 
+    "message": "Welcome to my notes application, use the /docs route to proceed"
+   }
+
+@app.put("/api")
+async def toggle(request: Request): 
+  return request.json()
+
+if __name__ == "__main__":
+  uvicorn.run("server.api:app", host="0.0.0.0", port=8000)

b43feaf7815cd9ba9fbedb83045963bce2ff948d
Phillip Logan <philo3194@gmail.com>
2023-02-14 18:48:07 -0500

    forgot to await

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ async def read_root():
 
 @app.put("/api")
 async def toggle(request: Request): 
-  return request.json()
+  return await request.json()
 
 if __name__ == "__main__":
   uvicorn.run("server.api:app", host="0.0.0.0", port=8000)
